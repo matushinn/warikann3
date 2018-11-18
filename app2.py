cb94737b21f8c7a49b344b3617b228fddd95eb0b
shotaro-ooe <shoooe97@gmail.com>
2018-11-18 11:54:19 +0900

    result check

---
+++ app2.py
@@ -0,0 +1,13 @@
+from app import warikan
+
+def check_warikan():
+    #ここに何か書く
+    result = warikan(1500,3) == "1人あたり: 500円, 端数: 0円"
+
+    if result == "True":
+        print("OK")
+    else:
+        print("NG")
+
+check_warikan():
+
